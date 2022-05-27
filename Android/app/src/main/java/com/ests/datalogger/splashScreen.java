4a83920d9b33567c03c8a74a7bc59682c7f300d6
Bouelkheir Yassine <31709973+yassinebouelkheir@users.noreply.github.com>
2022-05-27 19:47:22 +0100

    Update

---
+++ Android/app/src/main/java/com/ests/datalogger/splashScreen.java
@@ -0,0 +1,35 @@
+package com.ests.datalogger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread splashScreenStarter = new Thread() {
+            public void run() {
+                try {
+                    int delay = 0;
+                    while (delay < 2000) {
+                        sleep(150);
+                        delay = delay + 80;
+                    }
+                    startActivity(new Intent(splashScreen.this, MainActivity.class));
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    finish();
+                }
+            }
+
+        };
+        splashScreenStarter.start();
+    }
+}
