84e9a14df139b76fad088b03bff8d158515dea0d
Bouelkheir Yassine <31709973+yassinebouelkheir@users.noreply.github.com>
2022-06-17 21:31:31 +0100

    Many updates

--- Arduino/Arduino_Smarthouse_Center/Arduino_Smarthouse_Center.ino
+++ Arduino/Arduino_Smarthouse_Center/Arduino_Smarthouse_Center.ino
@@ -31,46 +31,26 @@
 #include <RF24_config.h>
 #include <string.h>
 #include <Keypad.h>
+#include "CO2Sensor.h"
 
-#define DHTTYPE DHT22
+#define DHTTYPE DHT11
 
 RF24 radio(9, 10);       
 const byte address1[6] = "63257";
-const byte address2[6] = "57369";
 
-DHT dhtext(53, DHTTYPE);
-DHT dhtint(52, DHTTYPE);
+DHT dhtext(4, DHTTYPE);
+DHT dhtint(5, DHTTYPE);
 
 bool cO2LevelHigh = false;
-bool cO2WindowClosed = true;
+bool cO2WindowClosed = true; 
 
-const int ROW_NUM = 4;
-const int COLUMN_NUM = 4;
-const String accessCodeNumber = "351629";
-
-char keys[ROW_NUM][COLUMN_NUM] = {
-   {'1','2','3', 'A'},
-   {'4','5','6', 'B'},
-   {'7','8','9', 'C'},
-   {'*','0','#', 'D'}
-};
-byte pin_rows[ROW_NUM] = {29, 28, 27, 26}; 
-byte pin_column[COLUMN_NUM] = {25, 24, 23, 22}; 
-Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);
-
-int numKeysPressed = 0;
-String numEntered = "";
-
-unsigned long timeoutTime = 0;
-unsigned long closeDoorTimeout = 0;
+CO2Sensor co2Sensor(A1, 0.99, 100);
 
 void setup() 
 {
    radio.begin();
 
-   radio.openReadingPipe(1, address2);
    radio.openWritingPipe(address1);
-//   radio.disableAckPayload();
 
    radio.setPALevel(RF24_PA_MAX); 
    radio.stopListening(); 
@@ -78,77 +58,27 @@ void setup()
    for(int i = 22; i < 30; i++) pinMode(i, OUTPUT);
    pinMode(2, OUTPUT);
    pinMode(3, OUTPUT);
-   digitalWrite(3, HIGH);
+   digitalWrite(3, LOW);
    digitalWrite(2, LOW);
    noTone(2);
-   pinMode(53, INPUT_PULLUP);
-   pinMode(52, INPUT_PULLUP);
+   pinMode(4, INPUT_PULLUP);
+   pinMode(5, INPUT_PULLUP);
+   dhtext.begin();
+   dhtint.begin();
+   co2Sensor.calibrate();
    Serial.begin(9600);
 }
 
 void loop() 
 {
-   radio.startListening();
-   delay(15);
-
-   if(radio.available())
-   {
-      char text[32];
-      radio.read(&text, sizeof(text));
-      Serial.println(text);
-   }
-
-   radio.stopListening();
-   delay(100);
-
-   if((millis()+30000) < timeoutTime)
-   {
-      numKeysPressed = 0;
-      numEntered = "";   
-   }
-
-   if((millis()+10000) < closeDoorTimeout)
-   {
-      char data[24];
-      Serial.println("setsensor 21 0"); 
-      closeDoor();
-   }
-
-   char key = keypad.getKey();
-   if(key)
-   {
-      numEntered = numEntered + String(key);
-      numKeysPressed++;
-      timeoutTime = millis();
-      //tone(2, 2000, 200);
-      if(numKeysPressed == 6)
-      {
-         if(/*!strcmp(numEntered, accessCodeNumber)*/1)
-         {
-            char data[24];
-            Serial.println("setsensor 21 1");
-            openDoor();
-            closeDoorTimeout = millis();
-            //tone(2, 5000, 1000);
-         }
-         else
-         {
-            //tone(2, 500, 1000);
-         }
-         numKeysPressed = 0;
-         numEntered = "";          
-      }
-   }
-
    char data[3];
    sprintf(data, "00");
-   //float cO2Level = 0.0;
    
    float tempext = dhtext.readTemperature();
    Serial.println("setsensor 14 " + String(tempext));
    float tempint = dhtint.readTemperature();
    Serial.println("setsensor 15 " + String(tempint));
-   float humidityint = dhtint.readHumidity();
+   float humidityint = 100 - dhtint.readHumidity();
    Serial.println("setsensor 16 " + String(humidityint));
 
    if(tempint > 24)
@@ -215,18 +145,18 @@ void loop()
 
    float brightness = analogRead(A0);
    Serial.println("setsensor 19 " + String(brightness));
-   if(brightness < 400)
+   if(brightness < 210)
    {
-      digitalWrite(3, LOW);
+      digitalWrite(3, HIGH);
       Serial.println("setsensor 20 1");
    }
    else 
    {
-      digitalWrite(3, HIGH);   
+      digitalWrite(3, LOW);   
       Serial.println("setsensor 20 0"); 
    }    
 
-   float cO2Level = 0.0;
+   float cO2Level = co2Sensor.read();
    
    Serial.println("setsensor 17 " + String(cO2Level));
    if(cO2Level > 600) 
